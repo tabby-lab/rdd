bc5399a320fa23ebcc25d88b21df83de0d8c6a22
Tabby Garcia <tabby.garcia1@gmail.com>
2020-12-16 13:37:33 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Person from './Person/Person'
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <h1> Hi, I'm a react app</h1>
+     <Person name="max" age="28"/>
+   </div>
+
+  );
+}
+
+export default App;
