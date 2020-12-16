44fe697c7ba9b05ec37b63c6b14b731a25db6d68
Tabby Garcia <tabby.garcia1@gmail.com>
2020-12-16 15:52:49 -0600

    change of files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Person from './Person/Person'
-import './App.css';
 
-function App() {
-  return (
-   <div>
-     <h1> Hi, I'm a react app</h1>
-     <Person name="max" age="28"/>
-   </div>
+import Ingredients from './components/Ingredients/Ingredients';
 
-  );
-}
+const App = props => {
+  return <Ingredients />;
+};
 
 export default App;
